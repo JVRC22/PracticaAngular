b52b0906fa43761b63a954eeae5bbb9ca65623cf
JVRC22 <javier.res220704@gmail.com>
2023-02-13 23:32:55 -0600

    Register funcional

--- src/app/Componentes/User/register/register.component.ts
+++ src/app/Componentes/User/register/register.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { Usuario } from 'src/app/Interfaces/usuario';
+import { UsuariosService } from 'src/app/Servicios/usuarios.service';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.css']
 })
-export class RegisterComponent {
+export class RegisterComponent implements OnInit {
+
+  form: FormGroup;
+
+  constructor(private usuariosService: UsuariosService, private fb: FormBuilder, private router: Router) {
+    this.form = this.fb.group({
+      nombre: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', Validators.required]
+    });
+   }
+
+  ngOnInit(): void {
+  }
+
+  onSubmit(usuario: Usuario) {
+    this.usuariosService.register(usuario).subscribe(response => console.log(response));
+    this.router.navigate(['/login']);
+  }
 
 }
